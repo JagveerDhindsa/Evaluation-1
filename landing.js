947b408b9edbc4b00bffbe154d725075f043f076
JagveerDhindsa <jagveerdhindsa6@gmail.com>
2024-10-25 11:58:49 +0530

    Added Contact Form Validation

--- landing.js
+++ landing.js
@@ -18,3 +18,16 @@ $(document).ready(function () {
 $('.navbar-container input[type="checkbox"]').on('change', function () {
     $('.menu-items').toggleClass('active');
 });
+
+$('.form-container .btn').on('click', function (event) {
+    event.preventDefault();
+    const name = $('input[type="text"]').val();
+    const email = $('input[type="email"]').val();
+    const message = $('textarea').val();
+    if (!name || !email || !message) {
+        alert('Please fill in all fields.');
+    } else {
+        alert('Thank you for your message, we will get back to you soon!');
+        $('input[type="text"], input[type="email"], textarea').val(''); // Clear fields
+    }
+});
